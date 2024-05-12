44ff0517444e7040a411da8f9f05ce2b6acf8d87
godo0209 <99616206+godo0209@users.noreply.github.com>
2024-05-13 00:35:45 +0200

    Particle system and material change
    Chest material change when entering zone and try some particle system

---
+++ Source/rvGame/CI_Interactable.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CI_Interactable.h"
+
+// Add default functionality here for any ICI_Interactable functions that are not pure virtual.
